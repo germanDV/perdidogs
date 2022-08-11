916d871606d2f17c4d11e87a511b06cae2969c30
German Viescas <germanviescas@gmail.com>
2022-08-11 18:53:15 -0300

    Set auth cookie to be SameSite=Strict

--- lib/auth/cookie.ts
+++ lib/auth/cookie.ts
@@ -4,7 +4,7 @@ const EXP = Number(process.env.AUTH_TOKEN_EXP_DAYS) || 1
 const expirationSeconds = EXP * 24 * 60 * 60
 
 export function getAuthCookie(token: string): string {
-  let cookieString = `auth_token=${token}; HttpOnly; Path=/; Max-Age=${expirationSeconds}`
+  let cookieString = `auth_token=${token}; HttpOnly; Path=/; SameSite=Strict; Max-Age=${expirationSeconds}`
 
   if (ENV === 'production') {
     cookieString += '; Secure'
@@ -15,7 +15,7 @@ export function getAuthCookie(token: string): string {
 
 export function expireCookie(): string {
   let cookieString =
-    'auth_token=perdidogs_user_signed_out; HttpOnly; Path=/; Max-Age=0; expires=Thu, 01 Jan 1970 00:00:00 GMT'
+    'auth_token=perdidogs_user_signed_out; HttpOnly; Path=/; SameSite=Strict; Max-Age=0; expires=Thu, 01 Jan 1970 00:00:00 GMT'
 
   if (ENV === 'production') {
     cookieString += '; Secure'
