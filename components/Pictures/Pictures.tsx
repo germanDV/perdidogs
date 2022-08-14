843de50a7a7717d22083c8b0efc1ed366b3c67aa
German Viescas <germanviescas@gmail.com>
2022-08-14 17:15:43 -0300

    Add stripe to change pictures

--- components/Pictures/Pictures.tsx
+++ components/Pictures/Pictures.tsx
@@ -1,16 +1,37 @@
 import Image from 'next/image'
+import { useState } from 'react'
+import styles from './Pictures.module.scss'
 
 type Props = {
   pictures: string[] | undefined
 }
 
 const Pictures = ({ pictures }: Props) => {
+  const [selected, setSelected] = useState(() => {
+    if (pictures && pictures.length > 0) {
+      return pictures[0]
+    }
+    return ''
+  })
+
   if (!pictures || pictures.length === 0) {
     return null
   }
 
   return (
-    <Image src={pictures[0]} alt="Foto" width="500" height="400" layout="responsive" priority />
+    <div className={styles.container}>
+      <Image src={selected} alt="Foto" width="500" height="400" layout="responsive" priority />
+
+      {pictures.length > 1 && (
+        <div className={styles.stripe}>
+          {pictures.map((p) => (
+            <div key={p} onClick={() => setSelected(p)}>
+              <Image src={p} alt="Foto" width="50" height="40" layout="responsive" />
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
   )
 }
 
